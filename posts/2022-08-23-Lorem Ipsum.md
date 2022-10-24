6fe3a84cf106e002835134e318ac947ba81343ce
aerphanas <muhamadaviv14@gmail.com>
2022-10-24 06:26:31 +0000

    update disign

--- posts/2022-08-23-Lorem Ipsum.md
+++ posts/2022-08-23-Lorem Ipsum.md
@@ -1,5 +1,5 @@
 ---
-title: Lorem Ipsum
+title: Lorem Ipsum 1
 author: aerphanas
 ---
 
--- site.hs
+++ site.hs
@@ -1,10 +1,19 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
+import  Data.Monoid (mappend)
+import  Hakyll
 
 
 --------------------------------------------------------------------------------
+myFeedConfiguration :: FeedConfiguration
+myFeedConfiguration = FeedConfiguration
+    { feedTitle       = "aerphanas blog"
+    , feedDescription = "blog untuk berbagi ilmu"
+    , feedAuthorName  = "aerphanas"
+    , feedAuthorEmail = "muhamadaviv14@gmail.com"
+    , feedRoot        = "aerphanas.github.io"
+    }
+
 config :: Configuration
 config = defaultConfiguration { destinationDirectory = "docs" }
 
@@ -18,7 +27,7 @@ main = hakyllWith config $ do
         route   idRoute
         compile compressCssCompiler
 
-    match "about.md" $ do
+    match "etc/about.md" $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
@@ -28,28 +37,14 @@ main = hakyllWith config $ do
         route $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= saveSnapshot "content"
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
-
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
 
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <-  (fmap (take 5) . recentFirst) =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
                     defaultContext
@@ -60,10 +55,33 @@ main = hakyllWith config $ do
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
+--------------------------------------------------------------------------------
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Arsip"            `mappend`
+                    defaultContext
 
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/post-list.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+
+    create ["atom.xml"] $ do
+        route idRoute
+        compile $ do
+            let feedCtx = postCtx `mappend`
+                    constField "description" "aerphanas blog update"
+    
+            posts <- fmap (take 10) . recentFirst =<< loadAllSnapshots "posts/*" "content"
+            renderAtom myFeedConfiguration feedCtx posts
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
+    dateField "date" "%b %d, %Y" `mappend`
     defaultContext
