8ce63a9844c908cb5d0da1cab549c6a86a9be72b
aerphanas <muhamadaviv14@gmail.com>
2022-11-09 06:43:14 +0700

    add hls

--- default.nix
+++ default.nix
@@ -6,12 +6,15 @@ let
   haskellDeps = ps: with ps; [
     base
     hakyll
+    haskell-language-server
   ];
   haskellEnv = haskell.packages.ghc924.ghcWithPackages haskellDeps;
 in mkShell {
   buildInputs = [
     haskellEnv
     haskellPackages.cabal-install
+    fira-code
+    fira-code-symbols
   ];
   shellHook = ''
       export PATH=$PATH:$HOME/.cabal/bin/
