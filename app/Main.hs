eeaeccf73975b613c4c90e9f2f44e4c330ba5ef3
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-05-07 16:52:40 +0700

    cleaning & refactor source code
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- app/Main.hs
+++ app/Main.hs
@@ -43,78 +43,92 @@ exposeFile patterns = match patterns $
                       route idRoute >>
                       compile copyFileCompiler
 
-main :: IO ()
-main = hakyllWith config $ do
-    exposeFile "robots.txt"
-    exposeFile "font/*"
-    exposeFile "images/*"
-    exposeFile "css/*"
-    exposeFile "404.html"
-    match "templates/*"  $ compile templateBodyCompiler
-    match "etc/about.md" $ do
-        route   $ setExtension   "html"
-        compile $ pandocCompiler                                  >>=
-          loadAndApplyTemplate   "templates/default.html" postCtx >>=
-          relativizeUrls
+exposeTemplate :: Rules ()
+exposeTemplate = match "templates/*" $ compile templateBodyCompiler
 
-    match "posts/*" $ do
-        route $ setExtension         "html"
-        compile $ do
-            let postWithOg =
-                    constField       "root"      root       <>
-                    openGraphField   "opengraph" postWithOg <>
-                    twitterCardField "twitter"   postWithOg <>
-                    defaultContext
-            pandocCompiler'                                            >>=
-              loadAndApplyTemplate "templates/post.html"    postCtx    >>=
-              saveSnapshot         "content"                           >>=
-              loadAndApplyTemplate "templates/default.html" postWithOg >>=
-              relativizeUrls
+exposeAbout :: Rules ()
+exposeAbout = match "etc/about.md" $ do
+              route   $ setExtension   "html"
+              compile $ pandocCompiler                                  >>=
+                loadAndApplyTemplate   "templates/default.html" postCtx >>=
+                relativizeUrls
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <-  loadAll "posts/*" >>= fmap (take 5) . recentFirst
-            let indexCtx = listField      "posts"     postCtx (return posts) <>
-                           constField     "root"      root                   <>
-                           openGraphField "opengraph" indexCtx               <>
-                           defaultContext
-            getResourceBody                                          >>=
-              applyAsTemplate indexCtx                               >>=
-              loadAndApplyTemplate "templates/default.html" indexCtx >>=
-              relativizeUrls
+exposePost :: Rules ()
+exposePost = match "posts/*" $ do
+             route $ setExtension         "html"
+             compile $ do
+                 let postWithOg = constField       "root"      root       <>
+                                  openGraphField   "opengraph" postWithOg <>
+                                  twitterCardField "twitter"   postWithOg <>
+                                  defaultContext
+                 pandocCompiler'                                            >>=
+                   loadAndApplyTemplate "templates/post.html"    postCtx    >>=
+                   saveSnapshot         "content"                           >>=
+                   loadAndApplyTemplate "templates/default.html" postWithOg >>=
+                   relativizeUrls
 
-    create ["archive.html"] $ do
+indexs :: Rules ()
+indexs = match "index.html" $ do
+         route idRoute
+         compile $ do
+             posts <-  loadAll "posts/*" >>= fmap (take 5) . recentFirst
+             let indexCtx = listField      "posts"     postCtx (return posts) <>
+                            constField     "root"      root                   <>
+                            openGraphField "opengraph" indexCtx               <>
+                            defaultContext
+             getResourceBody                                          >>=
+               applyAsTemplate indexCtx                               >>=
+               loadAndApplyTemplate "templates/default.html" indexCtx >>=
+               relativizeUrls
+
+styles :: Rules ()
+styles = create ["css/syntax.css"] $ route idRoute >> (compile . makeItem . styleToCss) pandocCodeStyle
+
+archives :: Rules ()
+archives = create ["archive.html"] $ do
         route idRoute
         compile $ do
             posts <- loadAll "posts/*" >>= recentFirst
-            let archiveCtx =
-                    listField  "posts"  postCtx (return posts)                                      <>
-                    constField "root"   root                                                        <>
-                    constField "title" "Arsip"                                                      <>
-                    constField "desc"  "Semua postingan yang ada diaerphanas bisa dilihat di sini"  <>
-                    defaultContext
-            makeItem ""                                                  >>=
+            let archiveCtx = listField  "posts"  postCtx (return posts) <>
+                             constField "root"   root                   <>
+                             constField "title" "Arsip"                 <>
+                             constField "desc"  "list post lama"        <>
+                             defaultContext
+            makeItem ""                                                     >>=
               loadAndApplyTemplate "templates/archive-list.html" archiveCtx >>=
-              loadAndApplyTemplate "templates/default.html"   archiveCtx >>=
+              loadAndApplyTemplate "templates/default.html"      archiveCtx >>=
               relativizeUrls
 
+sitemaps :: Rules ()
+sitemaps = create ["sitemap.xml"] $ do
+           route idRoute
+           compile $ do
+               posts <- loadAll "posts/*" >>= recentFirst
+               singlePages <- loadAll $ fromList ["etc/about.md"]
+               let pages = posts <> singlePages
+                   sitemapCtx = constField "root"  root <> listField  "pages" postCtx (return pages)
+               makeItem "" >>= loadAndApplyTemplate "templates/sitemap.xml" sitemapCtx
 
-    create ["sitemap.xml"] $ do
-        route idRoute
-        compile $ do
-            posts <- loadAll "posts/*" >>= recentFirst
-            singlePages <- loadAll $ fromList ["etc/about.md"]
-            let pages = posts <> singlePages
-                sitemapCtx = constField "root"  root <> listField  "pages" postCtx (return pages)
-            makeItem "" >>= loadAndApplyTemplate "templates/sitemap.xml" sitemapCtx
+atoms :: Rules ()
+atoms = create ["atom.xml"] $ route idRoute     >>
+  compile (loadAllSnapshots "posts/*" "content" >>=
+           fmap (take 10) . recentFirst         >>=
+           renderAtom myFeedConfiguration feedCtx)
+  where feedCtx = postCtx <> constField "description" "aerphanas blog update"
 
-    create ["atom.xml"] $ do
-        route idRoute
-        compile $ do
-            let feedCtx = postCtx <> constField "description" "aerphanas blog update"
-            posts <- loadAllSnapshots "posts/*" "content" >>= fmap (take 10) . recentFirst
-            renderAtom myFeedConfiguration feedCtx posts
+exposeFiles :: Rules ()
+exposeFiles = exposeFile "robots.txt" >> exposeFile "font/*" >>
+              exposeFile "images/*"   >> exposeFile "css/*"  >>
+              exposeFile "404.html"
 
-    create ["css/syntax.css"] $
-        route idRoute >> (compile . makeItem . styleToCss) pandocCodeStyle
+exposePages :: Rules ()
+exposePages = exposeFiles >> exposeTemplate >>
+              exposeAbout >> exposePost
+
+exposeStaticPages :: Rules ()
+exposeStaticPages = indexs   >> archives >>
+                    sitemaps >> atoms    >>
+                    styles
+
+main :: IO ()
+main = hakyllWith config $ exposeFiles >> exposePages >> exposeStaticPages
